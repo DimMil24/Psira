6a85877bbdab6e23d1cbb9be9b85f8cae5cc5d6d
Dimitris Milios <dimitris.milios24@gmail.com>
2025-05-11 14:08:12 +0300

    Add bad credentials exception handler

--- src/main/java/com/dimmil/bugtracker/exceptions/GlobalHandlerAdvice.java
+++ src/main/java/com/dimmil/bugtracker/exceptions/GlobalHandlerAdvice.java
@@ -4,6 +4,7 @@ import com.dimmil.bugtracker.exceptions.project.ProjectNotFoundException;
 import com.dimmil.bugtracker.exceptions.ticket.TicketNotFoundException;
 import com.dimmil.bugtracker.exceptions.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,6 +27,12 @@ public class GlobalHandlerAdvice {
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String userNotFoundHandler(UserNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    String badCredentialsHandler(BadCredentialsException ex) {
         return ex.getMessage();
     }
 }
