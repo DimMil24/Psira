153d8d2a63bcad226accaf9bafd6cb94bee667ed
Dimitris Milios <dimitris.milios24@gmail.com>
2025-05-17 14:56:03 +0300

    Add proper icons to sidebar

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -12,13 +12,20 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import { MoveToInbox, Mail } from "@mui/icons-material";
-import { projectTabs, ticketTabs } from "../utils/dashboardTabs";
 import { Link, Outlet, matchPath, useLocation } from "react-router";
 import { useContext } from "react";
 import { AuthContext } from "../auth/AuthProvider";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorPage from "./pages/error/ErrorPage";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import FolderCopyIcon from "@mui/icons-material/FolderCopy";
+import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import PortraitIcon from "@mui/icons-material/Portrait";
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import AssignmentLateIcon from "@mui/icons-material/AssignmentLate";
+import PersonIcon from "@mui/icons-material/Person";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const drawerWidth = 240;
 
@@ -55,72 +62,107 @@ export default function DashboardLayout() {
         <Toolbar />
         <Box sx={{ overflow: "auto" }}>
           <List>
-            {["Dashboard"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton
-                  selected={amISelected("/")}
-                  component={Link}
-                  to="/"
-                >
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
+            <ListItem key={"Dashboard"} disablePadding>
+              <ListItemButton
+                selected={amISelected("/")}
+                component={Link}
+                to="/"
+              >
+                <ListItemIcon>
+                  <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" />
+              </ListItemButton>
+            </ListItem>
           </List>
           <Divider />
         </Box>
         <Box sx={{ overflow: "auto" }}>
           <List>
-            {projectTabs.map((entry, index) => {
-              if (
-                (user?.role === "ROLE_SUBMITTER" ||
-                  user?.role === "ROLE_DEVELOPER") &&
-                entry.managersOnly
-              ) {
-                return;
-              }
-              return (
-                <ListItem key={entry.label} disablePadding>
-                  <ListItemButton
-                    selected={amISelected(entry.link)}
-                    component={Link}
-                    to={entry.link}
-                  >
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
-                    </ListItemIcon>
-                    <ListItemText primary={entry.label} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
+            <ListItem key={"All Projects"} disablePadding>
+              <ListItemButton
+                selected={amISelected("/projects")}
+                component={Link}
+                to={"/projects"}
+              >
+                <ListItemIcon>
+                  <FolderCopyIcon />
+                </ListItemIcon>
+                <ListItemText primary="All Projects" />
+              </ListItemButton>
+            </ListItem>
+            {(user?.role === "ROLE_ADMIN" || user?.role === "ROLE_MANAGER") && (
+              <ListItem key={"Create Project"} disablePadding>
+                <ListItemButton
+                  selected={amISelected("/projects/create")}
+                  component={Link}
+                  to={"/projects/create"}
+                >
+                  <ListItemIcon>
+                    <CreateNewFolderIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Create Project" />
+                </ListItemButton>
+              </ListItem>
+            )}
           </List>
           <Divider />
           <List>
-            {ticketTabs.map((entry, index) => (
-              <ListItem key={entry.label} disablePadding>
-                <ListItemButton
-                  selected={amISelected(entry.link)}
-                  component={Link}
-                  to={entry.link}
-                >
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
-                  </ListItemIcon>
-                  <ListItemText primary={entry.label} />
-                </ListItemButton>
-              </ListItem>
-            ))}
+            <ListItem key={"Open Tickets"} disablePadding>
+              <ListItemButton
+                selected={amISelected("/tickets/open")}
+                component={Link}
+                to={"/tickets/open"}
+              >
+                <ListItemIcon>
+                  <ImportContactsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Open Tickets" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem key={"My Tickets"} disablePadding>
+              <ListItemButton
+                selected={amISelected("/tickets/own")}
+                component={Link}
+                to={"/tickets/own"}
+              >
+                <ListItemIcon>
+                  <PortraitIcon />
+                </ListItemIcon>
+                <ListItemText primary="My Tickets" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem key={"Resolved Tickets"} disablePadding>
+              <ListItemButton
+                selected={amISelected("/tickets/resolved")}
+                component={Link}
+                to={"/tickets/resolved"}
+              >
+                <ListItemIcon>
+                  <AssignmentTurnedInIcon />
+                </ListItemIcon>
+                <ListItemText primary="Resolved Tickets" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem key={"Submit Ticket"} disablePadding>
+              <ListItemButton
+                selected={amISelected("/tickets/submit")}
+                component={Link}
+                to={"/tickets/submit"}
+              >
+                <ListItemIcon>
+                  <AssignmentLateIcon />
+                </ListItemIcon>
+                <ListItemText primary="Submit Ticket" />
+              </ListItemButton>
+            </ListItem>
           </List>
           <Divider />
           <List>
             <ListItem key="account" disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  <Mail />
+                  <PersonIcon />
                 </ListItemIcon>
                 <ListItemText primary={user?.fullName} />
               </ListItemButton>
@@ -129,7 +171,7 @@ export default function DashboardLayout() {
             <ListItem key="logout" disablePadding>
               <ListItemButton onClick={() => logout()}>
                 <ListItemIcon>
-                  <Mail />
+                  <LogoutIcon />
                 </ListItemIcon>
                 <ListItemText primary="Logout" />
               </ListItemButton>
